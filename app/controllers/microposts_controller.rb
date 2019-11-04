8adccd8d24b5db2f9f5c65cbdfaf2790067671cf
nguyentrison2585 <nguyentrison.33.10a3@gmail.com>
2019-11-04 15:31:35 +0700

    Chapter 13: User microposts

---
+++ app/controllers/microposts_controller.rb
@@ -0,0 +1,41 @@
+class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: %i(create destroy)
+  before_action :find_post, only: :destroy
+
+  def create
+    @micropost = current_user.microposts.build micropost_params
+    @micropost.image.attach params[:micropost][:image]
+    save_post
+    redirect_to root_url
+  end
+
+  def destroy
+    if @micropost.destroy
+      flash[:seccess] = t "post_deleted"
+    else
+      flash[:danger] = t "post_delete_fail"
+    end
+    redirect_to request.referer || root_url
+  end
+
+  private
+
+  def micropost_params
+    params.require(:micropost).permit %i(content image)
+  end
+
+  def save_post
+    if @micropost.save
+      flash[:success] = t "post_created"
+    else
+      @feed_items = current_user.feed.page(params[:page])
+                                .per Settings.posts_per_page
+      flash[:danger] = t "post_create_fail"
+    end
+  end
+
+  def find_post
+    @micropost = current_user.microposts.find_by id: params[:id]
+    redirect_to root_url if @micropost.nil?
+  end
+end
